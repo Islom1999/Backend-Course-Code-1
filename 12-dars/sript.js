c04cd17b15ba6dade411da695ef1e61af92581f5
Islom1999 <islomkarimovalisherovich@gmail.com>
2022-06-23 20:02:57 +0500

    lesson 12 and 13 loaded

---
+++ 12-dars/sript.js
@@ -0,0 +1,161 @@
+/*
+console.log('Salom')
+console.log(500)
+console.log('500')
+*/ 
+
+/*
+let name = 'Islom'    // string
+let age = 23          // number
+let isMarred = false  // boolean => false - yolgon , true - rost
+let surName           // undefined - topilmadi
+let email = null      // bosh qiymat
+
+console.log('Islom')
+console.log(name)
+console.log(age)
+console.log(2022-age)
+console.log(isMarred)
+console.log(surName)    // undefined
+console.log(email)      // bosh qiymat
+
+*/  
+
+/*
+var ism = null 
+var ism1 = 'Islom' 
+
+let number1 = 1
+let number2 = 2
+
+// ism familiya
+
+let ismFamiliya  // 
+*/ 
+
+// get post by id
+
+// let getPostById
+
+/*
+let str1 = 'Islom'
+let str2 = "Karimov"
+let str3 = `${str1} ${str2} Alisherovich` 
+
+console.log(str1)
+console.log(str2)
+console.log(str3)
+*/ 
+
+/*
+let name1 = 'Islom'
+let name2 = name1
+
+console.log(name2)
+*/ 
+
+/*
+let a = 5
+a = 6
+console.log(a)
+a = 8
+console.log(a)
+*/ 
+
+/*
+let a = 5
+a = a + 5
+console.log(a)
+*/ 
+
+/*
+let a = 5
+a += 5      /// a= a+5
+console.log(a)
+*/
+/* 
+let a = 5
+a += 5      /// a = a + 5
+a -= 5      /// a = a - 5
+a *= 5      /// a = a * 5
+a /= 5      /// a = a / 5
+a %= 5      /// a = a % 5
+a **= 5      /// a = a ** 5 = 5*5*5*5*5
+
+console.log(a)
+*/ 
+/*
+let a = 50
+let b = 10
+let c = a + b   // 60
+
+console.log(c)
+*/ 
+/*
+let a = 50
+let b = 10
+
+console.log( 'a=', a, 'b=', b )
+
+console.log( a+b )
+console.log( a-b )
+console.log( a*b )
+console.log( a/b )
+console.log( a%b )
+console.log( a**b )
+
+// 55 = 10*5 + 5  => 50%10=5
+*/ 
+/*
+let bool1 = 5 > 3  // true
+let bool2 = 5 < 3  // false 
+let bool3 = 5 > 5  // false 
+let bool4 = 5 >= 5 // true 
+let bool5 = 5 <= 5 // true 
+let bool6 = 5 == 5 // true 
+let bool7 = 5 == '5'  // true 
+let bool8 = 5 === '5'  // false 
+let bool9 = 5 !== 4  // true 
+let bool10 = 5 !== 5  // false 
+
+
+console.log(bool10)
+*/ 
+
+/*
+let num = 5
+let str = '5'
+let bool = '5' == 5  // true boolean
+
+let type = typeof num  // 'number'
+console.log(type)
+
+console.log(typeof str)
+console.log(typeof bool)
+*/ 
+
+// alert('Saytga hush kelibsiz')  // xabar yetkazish
+
+// prompt('Ismingizni kiriting') // malumot kiritish
+
+/*
+let name = prompt('Ismingizni kiriting')
+let surName = prompt('Familiyagizni kiriting')
+console.log(name, surName)
+*/ 
+/*
+let str1 = 'Salom'
+let str2 = 'Islom'
+let str3 = str1 + ' ' + str2
+console.log(str3)
+*/ 
+
+
+
+
+
+
+
+
+
+
---
+++ 13-dars/js/script.js
@@ -0,0 +1,240 @@
+/*
+let 
+var
+const
+*/ 
+/*
+let a = 5
+a=10
+console.log(a)
+
+const b = 10
+b = 20
+console.log(b)
+*/ 
+
+// 1 => true; 0 => false
+// mantiqiy kopaytirish (va, and, u)
+// 1 && 1 = 1
+// 1 && 0 = 0
+// 0 && 1 = 0
+// 0 && 0 = 0
+/*
+let bool = 5 > 3 //true
+
+//  0 < b < 100
+let bool1 = b>0 && b<100  // hammasi rost bolganda rost
+
+
+// mantiqiy qoshish (yoki, or, yesli)
+// 1 || 1 = 1
+// 1 || 0 = 1
+// 0 || 1 = 1
+// 0 || 0 = 0
+
+// b<0  b>100
+
+let bool2 = b<0 || b>100  // hammasi yolgon bolganda yolgon
+
+// mantiqiy inkor ! (emas) 
+// b=5
+let bool3 = b!==5
+*/
+/*
+let bool4 = !true
+let bool5 = !false
+let bool6 = !5>2
+console.log(bool4)  // false
+console.log(bool5)  // true
+console.log(bool6)  // false
+*/ 
+
+//let a = 5
+//a = a + 1
+//a+=1
+// a++ //inc
+// a--
+//++a
+//--a
+/*
+console.log(a++)
+console.log(a)
+*/ 
+// console.log(++a)
+
+/*
+let str1 = '5'
+let str2 = '10'
+
+console.log(str1 + str2)
+*/
+
+/*
+let str1 = parseInt('5.5')  // integer  => 5
+let str2 = parseFloat('10.5')  // float => 10.5
+
+console.log(str1 + str2)
+*/ 
+/*
+let str1 = Number('5.5')  //   => 5.5
+let str2 = Number('10.5')  //  => 10.5
+
+console.log(str1 + str2)
+*/
+/*
+let str1 = +'5.5'  //   => 5.5
+let str2 = '10.5'*1  //  => 10.5
+
+console.log(str1 + str2)
+*/ 
+/*
+let str1 = Number('5.5sdsaasds') // NAN => son emas
+str1 = parseInt('5.9sdsaasds') //  => 5
+str1 = parseFloat('5.5.6sdsaasds') //  => 5.5
+
+console.log(str1)
+*/ 
+/*
+let num = Math.ceil(5.00001) // yuqoriga
+num = Math.floor(5.99999999) // pastga
+num = Math.round(5.99999999) // yaqiniga 6
+num = Math.round(5.29999999) // yaqiniga 5
+num = Math.round(5.5) // yaqiniga 6
+num = Math.round(5.4999) // yaqiniga 5
+num = Math.abs(-5.4999) // yaqiniga 5
+num = Math.abs(5.4999) // yaqiniga 5
+console.log(num)
+*/ 
+/*
+let num = Math.sqrt(4)  // kvadrat ildiz
+num = Math.sqrt(5)  // kvadrat ildiz
+num = Math.pow(2,3) // daraja 2**3
+num = Math.pow(125, 1/3)  // daraja 
+
+console.log(num)
+*/ 
+/*
+let max = Math.max(3,2,52,63,12,47)
+let min = Math.min(3,2,52,63,12,47)
+console.log(min)
+*/ 
+/*
+let num = Math.random()  // tax son => 0 < r < 1
+console.log(num)
+*/ 
+/*
+let num = Math.ceil(Math.random()*5) 
+console.log(num)
+*/ 
+/*
+let name = 'Go\'zal'
+name = "Go'zal"
+console.log(name)
+*/ 
+/*
+let name='Islom'
+let surname="Karimov"
+let age=23
+
+let str = `Name: ${name}, SurName: ${surname}, Age: ${age}`
+console.log(str)
+*/ 
+  /*        //01234
+let name = 'Islom'
+
+console.log(name[0])
+console.log(name[4])
+console.log(name[6])
+*/
+/*
+let name = 'Islom Karimov'
+name.toUpperCase()
+console.log( name.length )
+console.log( name.toUpperCase() )
+console.log( name.toLowerCase() )
+console.log(name)
+*/ 
+/*
+let pass = 'Islom'
+let bool = pass == "Islom" // true
+*/ 
+/*
+if(true){
+    console.log('Rost')
+}else{
+    console.log('Yolgon')
+}
+*/ 
+/*
+let a = 5
+
+if( a % 2 == 0 ){
+    console.log('juft')
+}else{
+    console.log('toq')
+}*/ 
+
+/*
+let a = +prompt('son kirit')
+
+if( a > 0 ){
+    console.log('musbat')
+}else if( a==0 ){
+    console.log('nolga teng')
+}else{
+    console.log('manfiy')
+}
+*/ 
+/*
+let a = +prompt('son kirit')
+
+if( a > 0 ){
+    console.log('musbat')
+}else if(a == '' ){
+    console.log('siz son kiritmadiz')
+}else if( a==0 ){
+    console.log('nolga teng')
+}else{
+    console.log('manfiy')
+}*/ 
+/*
+if(){
+
+}
+if(){
+    
+}
+
+if(){
+    
+}else{
+
+}
+
+if(){
+    
+}else if(){
+    
+}
+else if(){
+    
+}
+else if(){
+    
+}
+else{
+    
+}
+*/ 
+
+
+
+
+
+
+
+
+
+
+
+
