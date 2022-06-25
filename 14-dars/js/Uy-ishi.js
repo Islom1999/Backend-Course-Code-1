642987d01a43882e42b31ba10c46b94a82d86270
Islom1999 <islomkarimovalisherovich@gmail.com>
2022-06-25 20:03:51 +0500

    lesson 14 loaded

---
+++ 14-dars/js/Uy-ishi.js
@@ -0,0 +1,139 @@
+
+// 1
+/*
+let a = 5
+if(a > 0){
+    console.log('musbat')
+}
+*/ 
+
+// 2 
+/*
+let a = 5
+if(a % 2 == 0){
+    console.log('juft')
+}else{
+    console.log('toq')
+}
+*/ 
+/*
+// 4
+let a = -5
+let b = -10
+
+if(a>0 && b>0){
+    console.log(true)
+}else{
+    console.log(false)
+}
+*/ 
+/*
+// 5  => a>b,  b>c = a>b>c => 
+let [a,b,c] = [ 10, 9, 8]
+
+if(a>b && b>c){
+    console.log(true)
+}else{
+    console.log(false)
+}
+*/ 
+
+/*
+//7 a,b => ikalasiyam toq
+
+let [a,b] = [6,4]
+
+if(a%2==1 && b%2==1){
+    console.log('ikalasiyam toq')
+}else{
+    console.log('ikalasiyam toq emas')
+}
+*/ 
+
+/*
+// 9 => a>0,b>0 => a>0,c>0 => b>0, c>0
+let [a,b,c] = [-6,-4,-5]
+
+if(a>0 && b>0 && c>0){
+    console.log('barchasi musbat')
+}else if(a>0 && b>0){
+    console.log('ikkasi musbat')
+}else if(a>0 && c>0){
+    console.log('ikkasi musbat')
+}else if(b>0 && c>0){
+    console.log('ikkasi musbat')
+}else if(b>0 || c>0 || a>0){
+    console.log('bittasi musbat')
+}else{
+    console.log('hammasi musbat emas')
+}
+*/ 
+
+/*
+let [a,b] = [6,7]
+
+if(a>b){
+    console.log(a+' katta ' +b+ ' dan')
+}
+if(b>a){
+    console.log(b+' katta ' +a+ ' dan')
+}
+*/ 
+
+/*
+let [a,b,c] = [8,8,8]
+
+if(a>b && a>c){
+    console.log(a, ' a son katta')
+}
+if(b>a && b>c){
+    console.log(b, ' b son katta')
+}
+if(c>a && c>b){
+    console.log(c, ' c son katta')
+}
+*/ 
+
+/*
+let a = -7
+
+if(a>0 && a%2==0){
+    console.log("juft musbat")
+}else if(a>0 && a%2==1){
+    console.log("toq musbat")
+}else if(a<0 && a%2==-1){
+    console.log("toq manfiy")
+}else if(a<0 && a%2==0){
+    console.log("juft manfiy")
+}
+*/ 
+/*
+let [a,b] = [8,4]
+
+if(a%2==1 && b%2==0){
+    console.log('bittasi toq')
+}
+if(a%2==0 && b%2==1){
+    console.log('bittasi toq')
+}
+if(a%2==1 && b%2==1){
+    console.log('ikalasiyam toq')
+}
+if(a%2==0 && b%2==0){
+    console.log('ikalasiyam juft')
+}
+*/ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 14-dars/js/script.js
@@ -0,0 +1,62 @@
+
+//let str = 'Islom a Karimov          a'
+
+//console.log(str)
+//console.log( str.substr(6, 1) )  // index, nechta belgi
+//console.log( str.substring(1, 5) )  // index bosh, index tugashi 
+//console.log( str.substring(1, 5) )  // index bosh, index tugashi 
+
+//console.log(str.split(' '))
+
+//console.log(str.trim())
+//console.log(str.includes('q'))
+// console.log(str.replace('Islom', 'Asomiddin')) // izlonadigan, o'zgartiradigan
+
+//console.log(str.charAt(7))
+//console.log(str[7])
+//console.log(str.charCodeAt(0)) // 
+
+//console.log(str.indexOf('Karim')) 
+//console.log(str.indexOf('a'))   // 
+// console.log(str.lastIndexOf('a'))   // 
+
+/*
+let name = 'Asilbek '
+let surName = ' Eshqurbonov'
+
+console.log( name.concat(surName) )
+console.log( name + surName )
+console.log(name.startsWith('s'))
+console.log(name.endsWith('ek'))
+console.log(name.search('ek'))
+console.log(name.repeat(10))
+*/ 
+/*
+let bool = Boolean(-1)   // 0 => false
+let bool1 = Boolean('')   // '', "" ,'' => false
+let bool2 = Boolean(undefined)  
+let bool3 = Boolean(null)  
+let bool4 = Boolean(true)  
+let bool6 = Boolean(false)  
+let bool5 = Boolean('false')  
+
+console.log(bool6)
+*/ 
+
+let a = prompt('son kirit')
+
+if(a){
+    console.log(+a+5)
+}else{
+    console.log('siz hech narsa kiritmadiz')
+}
+
+
+
+
+
+
+
+
+
+
